f5ad6cb79d3a11ebb6672715c41fd8229fececf2
Bilaal Salman <mbilaalme@gmail.com>
2024-01-25 10:31:51 +0100

    refactor: Changed FlatButton to TextButton.

--- lib/screens/alero/components/button.dart
+++ lib/screens/alero/components/button.dart
@@ -5,9 +5,7 @@ class RoundedButton extends StatelessWidget {
   final Function press;
   final Color color, textColor;
 
-  const RoundedButton(
-      {Key key, this.text, this.press, this.color, this.textColor})
-      : super(key: key);
+  const RoundedButton({Key key, this.text, this.press, this.color, this.textColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,26 +14,50 @@ class RoundedButton extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 30),
       width: size.width * 0.9,
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(30),
-        child: FlatButton(
-          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-          color: color,
-          onPressed: press,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                text,
-                style: TextStyle(
-                    color: textColor,
-                    fontSize: 16.0,
-                    fontFamily: 'Poppins-Regular'),
+          borderRadius: BorderRadius.circular(30),
+          child: TextButton(
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all(
+                EdgeInsets.symmetric(vertical: 15, horizontal: 40),
               ),
-              SizedBox(width: 10.0),
-            ],
+              backgroundColor: MaterialStateProperty.all(color),
+              foregroundColor: MaterialStateProperty.all(textColor),
+            ),
+            onPressed: press,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontFamily: 'Poppins-Regular',
+                  ),
+                ),
+                SizedBox(width: 10.0),
+              ],
+            ),
+          )
+
+          // child: FlatButton(
+          //   padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+          //   color: color,
+          //   onPressed: press,
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       Text(
+          //         text,
+          //         style: TextStyle(
+          //             color: textColor,
+          //             fontSize: 16.0,
+          //             fontFamily: 'Poppins-Regular'),
+          //       ),
+          //       SizedBox(width: 10.0),
+          //     ],
+          //   ),
+          // ),
           ),
-        ),
-      ),
     );
   }
 }
